97ab00625494b7225a5cecc8cdd424d911b638d9
AlexThomHab <alex.haberfield@gmail.com>
2025-09-18 22:07:29 +0100

    add component for int row
    add component for interval row

---
+++ src/app/component/interval-row/interval-row.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interval-row',
+  imports: [],
+  templateUrl: './interval-row.html',
+  styleUrl: './interval-row.css'
+})
+export class IntervalRow {
+
+}
